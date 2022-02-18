d7f2bf559559e79691f301867ed2d38052145519
Preeti <nanubasyal@gmail.com>
2022-02-18 09:25:01 -0500

    Mpdels Created

--- import_data/models.py
+++ import_data/models.py
@@ -11,7 +11,7 @@ class New_Data(models.Model):
     date_of_birth=models.DateField()
     gender=models.CharField(max_length=10)
     salary=models.DecimalField(max_digits=5, decimal_places=2)
-    designation=models.CharField(max_length=50)
+    designation=models.CharField(max_length=150)
     imported_by=models.CharField(max_length=100)
     imported_at=models.DateTimeField(auto_now=True,auto_now_add=False)
 
